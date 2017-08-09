70f16aff7982825e2fcb0c52363290c2c347ed3e
broosted <matrix.10.tad@gmail.com>
2017-08-08 21:50:51 -0700

    fixed the rating

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input} from '@angular/core';
+import {Rating} from './rating.modle';
 
 
 @Component({
@@ -8,9 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RatingComponent implements OnInit {
 
-  constructor() { }
+
+
+  values : Rating[];
+  startypes :string[];
+  index: number; //this will be the index of the value
+
+
+                                       
+  constructor() { 
+    this.startypes = [
+      'ion-ios-star', //star full
+      'ion-ios-star-half', //star half
+      'ion-ios-star-outline' //star empty
+    ];
+    
+    let b = [
+      { id: 1, class_str: 'ion-ios-star-outline'}
+      ,{id: 2,class_str: 'ion-ios-star-outline'},
+      {id: 3, class_str: 'ion-ios-star-outline'},
+      {id:4,class_str:'ion-ios-star-outline'},
+      {id:5,class_str:'ion-ios-star-outline'}
+    ];
+
+    this.values = b;
+
+  }
 
   ngOnInit() {
   }
+
+
+  indexClick(event, item: Rating): void{
+
+    for(let i =0;i<this.values.length;i++){
+      this.values[i].class_str = 'ion-ios-star-outline';
+    }
+    
+    
+    this.index = item.id;
+    
+    for(let i= this.index-1 ; i >= 0;i--){
+      this.values[i].class_str = 'ion-ios-star';
+
+    }
+
+  }
+
+
+  
+
+
 
 }
---
+++ src/app/rating/rating.modle.ts
@@ -0,0 +1,13 @@
+export class Rating{
+    id: number;
+    class_str : string;
+
+
+    constructor(id: number,class_str: string){
+        this.id = id;
+        this.class_str = class_str;
+
+    }
+
+  
+}
